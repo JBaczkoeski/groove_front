645d7a8c25d685184895caf40ff9f18481c23b49
JBaczkowski <baczkowskikuba2@gmail.com>
2023-10-12 18:51:00 +0200

    init

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
 // import widoków
-import Home from '@/pages/HomePage.vue'
+import HomePage from "@/pages/HomePage.vue";
 import LoginPage from '@/pages/LoginPage.vue'
 import RegisterPage from "@/pages/RegisterPage.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: Home },
+        { path: '/', component: HomePage },
         { path: '/logowanie', component: LoginPage },
         { path: '/rejestracja', component: RegisterPage },
     //     Tutaj bartuś będziemy dodawać nowe ścieżki na razie chuja tu jest
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,30 +1,12 @@
 <template>
-  <h1 class="text-center text-bg-danger text-warning">Tu trzeba zrobić nie samowitą stronę główną </h1>
+  <div>
+
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: 'Hello_world',
-  props: {
-    msg: name
-  }
+  name: 'HomePage',
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
