f1b50adef3fb8e84e354c3ad5c2c781b89df83df
BartoszBoguszQ <bartosz.b.bogusz@gmail.com>
2023-10-14 18:25:26 +0200

    carousela z pizdy rock bottom level

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,37 +1,53 @@
 <template>
-  <div class="container col-5 shadow d-flex justify-content-center align-items-center flex-column container-flex">>
-    <h3 class="mt-5">Strona Główna</h3>
-    <!-- Carousel -->
-    <div id="demo" class="carousel slide" data-bs-ride="carousel">
+  <div class="mb-5 container col-5 shadow d-flex justify-content-center align-items-center flex-column container-flex">
+    <h3 class="mt-1">Strona Główna</h3>
 
-      <!-- Znaczniki -->
-      <div class="carousel-indicators">
-        <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
-        <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-        <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
-      </div>
+<!--    &lt;!&ndash; Carousel &ndash;&gt;-->
+<!--    <div id="demo" class="carousel slide" data-bs-ride="carousel">-->
 
-      <!-- Slajdy -->
-     <!-- <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img src="" alt="Los Angeles" class="d-block w-100">
-        </div>
-        <div class="carousel-item">
-          <img src="" alt="Chicago" class="d-block w-100">
-        </div>
-        <div class="carousel-item">
-          <img src="" alt="New York" class="d-block w-100">
-        </div>
-      </div>
-    -->
-      <!-- '<-- -->' -->
-      <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon"></span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-        <span class="carousel-control-next-icon"></span>
-      </button>
-    </div>
+<!--      &lt;!&ndash; Znaczniki &ndash;&gt;-->
+<!--      <div class="carousel-indicators">-->
+<!--        <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>-->
+<!--        <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>-->
+<!--        <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>-->
+<!--      </div>-->
+
+<!--      &lt;!&ndash; Slajdy &ndash;&gt;-->
+<!--      <div class="carousel-inner">-->
+<!--        <div class="carousel-item active">-->
+<!--          <img src="https://static.posters.cz/image/750/paradise-on-earth-i161095.jpg" alt="Los Angeles"-->
+<!--               class="d-block w-100">-->
+<!--          <div class="carousel-caption text-dark">-->
+<!--            <h3>Tytuł</h3>-->
+<!--            <p> ASDASDASDASDAS ASDASDASD ASDASDAS ASDASDA SA DASD ASD ASD ASD ASD ASD ASD </p>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <div class="carousel-item">-->
+<!--          <img src="https://images.photowall.com/products/78066/waterfall-paradise.jpg?h=699&q=85" alt="Chicago"-->
+<!--               class="d-block w-100">-->
+<!--          <div class="carousel-caption text-dark">-->
+<!--            <h3>Tytuł</h3>-->
+<!--            <p> ASDASDASDASDAS ASDASDASD ASDASDAS ASDASDA SA DASD ASD ASD ASD ASD ASD ASD </p>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <div class="carousel-item">-->
+<!--          <img src="https://images.photowall.com/products/56862/tropical-paradise-1.jpg?h=699&q=85" alt="New York"-->
+<!--               class="d-block w-100">-->
+<!--          <div class="carousel-caption text-dark">-->
+<!--            <h3>Tytuł</h3>-->
+<!--            <p> ASDASDASDASDAS ASDASDASD ASDASDAS ASDASDA SA DASD ASD ASD ASD ASD ASD ASD </p>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+
+
+<!--      <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">-->
+<!--        <span class="carousel-control-prev-icon"></span>-->
+<!--      </button>-->
+<!--      <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">-->
+<!--        <span class="carousel-control-next-icon"></span>-->
+<!--      </button>-->
+<!--    </div>-->
   </div>
 </template>
 
