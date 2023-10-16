3383d095009480a6f16fb01b0bd9735f391a416f
BartoszBoguszQ <bartosz.b.bogusz@gmail.com>
2023-10-16 20:12:54 +0200

    prototyp do obootstrapowania

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,54 +1,41 @@
 <template>
-  <div class="mb-5 container col-5 shadow d-flex justify-content-center align-items-center flex-column container-flex">
-    <h3 class="mt-1">Strona Główna</h3>
-
-<!--    &lt;!&ndash; Carousel &ndash;&gt;-->
-<!--    <div id="demo" class="carousel slide" data-bs-ride="carousel">-->
-
-<!--      &lt;!&ndash; Znaczniki &ndash;&gt;-->
-<!--      <div class="carousel-indicators">-->
-<!--        <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>-->
-<!--        <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>-->
-<!--        <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>-->
-<!--      </div>-->
-
-<!--      &lt;!&ndash; Slajdy &ndash;&gt;-->
-<!--      <div class="carousel-inner">-->
-<!--        <div class="carousel-item active">-->
-<!--          <img src="https://static.posters.cz/image/750/paradise-on-earth-i161095.jpg" alt="Los Angeles"-->
-<!--               class="d-block w-100">-->
-<!--          <div class="carousel-caption text-dark">-->
-<!--            <h3>Tytuł</h3>-->
-<!--            <p> ASDASDASDASDAS ASDASDASD ASDASDAS ASDASDA SA DASD ASD ASD ASD ASD ASD ASD </p>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--        <div class="carousel-item">-->
-<!--          <img src="https://images.photowall.com/products/78066/waterfall-paradise.jpg?h=699&q=85" alt="Chicago"-->
-<!--               class="d-block w-100">-->
-<!--          <div class="carousel-caption text-dark">-->
-<!--            <h3>Tytuł</h3>-->
-<!--            <p> ASDASDASDASDAS ASDASDASD ASDASDAS ASDASDA SA DASD ASD ASD ASD ASD ASD ASD </p>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--        <div class="carousel-item">-->
-<!--          <img src="https://images.photowall.com/products/56862/tropical-paradise-1.jpg?h=699&q=85" alt="New York"-->
-<!--               class="d-block w-100">-->
-<!--          <div class="carousel-caption text-dark">-->
-<!--            <h3>Tytuł</h3>-->
-<!--            <p> ASDASDASDASDAS ASDASDASD ASDASDAS ASDASDA SA DASD ASD ASD ASD ASD ASD ASD </p>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-
-
-<!--      <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">-->
-<!--        <span class="carousel-control-prev-icon"></span>-->
-<!--      </button>-->
-<!--      <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">-->
-<!--        <span class="carousel-control-next-icon"></span>-->
-<!--      </button>-->
-<!--    </div>-->
+  <div class="baner mt-4 justify-content-center">
+    <img src="../assets/baner.png">
   </div>
+
+  <hr class="divider">
+
+  <div class="row">
+    <div class="col-md-7">
+
+      <h2> Prosty i intuicyjny odtwarzacz</h2>
+      <p class="lead">
+        Nasza aplikacja posiada prosty i intuicyjny odtwarzacz.
+      </p>
+    </div>
+    <div class="col-md-5"><img class="img-fluid mx-auto"
+                               src="https://elfsight.com/pl/wp-content/uploads/sites/7/2021/03/audio-player-hero-image.png">
+    </div>
+  </div>
+
+  <hr class="divider">
+
+  <div class="row">
+    <div class="col-md-7"><img class="photo_2 img-fluid mx-auto"
+                               src="https://i0.wp.com/polskaplyta-polskamuzyka.pl/wp-content/uploads/2023/01/Najlepsze-rapowe-plyty-2022-roku-RANKING.png?fit=800%2C800&ssl=1">
+    </div>
+    <div class="col-md-5">
+
+      <h2> Ogromna baza albumów</h2>
+      <p class="lead">
+        Nasza aplikacja posiada ogromną bazę albumów.
+      </p>
+    </div>
+
+  </div>
+
+  <hr class="divider">
+
 </template>
 
 <script>
@@ -57,3 +44,12 @@ export default {
   name: 'HomePage',
 }
 </script>
+
+<style>
+  .photo_2{
+    width:352px;
+    height:217px;
+  }
+
+
+</style>
