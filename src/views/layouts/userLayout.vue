f380462776621a8f8c41b89e8c45ead8bf10226d
JBaczkowski <baczkowskikuba2@gmail.com>
2023-10-26 18:14:06 +0200

    zdjęcie użytkownika

--- src/views/layouts/userLayout.vue
+++ src/views/layouts/userLayout.vue
@@ -35,12 +35,20 @@
             </li>
           </ul>
         </div>
-        <div class="container col-3 d-flex justify-content-end" v-if="isLogged">
+        <div class="container col-1 d-flex justify-content-end" v-if="isLogged">
+          <ul class="navbar-nav">
+
+          </ul>
+        </div>
+        <div class="container col-2 d-flex justify-content-end" v-if="isLogged">
           <ul class="navbar-nav">
             <li class="nav-item">
+              <img class="user-profile" src="https://scontent-waw1-1.xx.fbcdn.net/v/t1.6435-9/103488860_1966332453498047_1673215677078360732_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=be3454&_nc_ohc=rmDTA4aETMoAX_qeicJ&_nc_ht=scontent-waw1-1.xx&oh=00_AfAyw3rAT-5ug63--8tht5KkIln2olXMLrkOemt4v65FvA&oe=656206C2" alt="Oskar">
+            </li>
+            <li class="nav-item mt-2">
               <NavLink :to="'/konto'" :label="'Konto'"/>
             </li>
-            <li class="nav-item">
+            <li class="nav-item mt-2">
               <button class="nav-link" @click="logout">Wyloguj</button>
             </li>
           </ul>
@@ -78,5 +86,11 @@ export default {
 <style>
 .logo {
   width: 60px;
+}
+.user-profile {
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  object-fit: cover;
 }
 </style>
