e079f348b4c89e99c6347868eeb9bc9c8e8c2182
JBaczkowski <baczkowskikuba2@gmail.com>
2023-12-06 16:45:31 +0100

    drobne poprawki wszystkiego pamiętaj musisz kuba poprawić navbar w adminie

--- src/components/SideBarUser.vue
+++ src/components/SideBarUser.vue
@@ -1,32 +1,32 @@
 <template>
-  <div class="container-fluid flex-column p-3 side-bar-border side-bar-st">
+  <div class="container-fluid p-3 side-bar-border side-bar-st">
     <ul class="nav nav-pills flex-column mb-auto mt-5">
       <li class="nav-item text-start">
-        <a href="/konto" class="nav-link mt-3 mb-2" aria-current="page">
+        <a href="/konto" class="nav-link mt-3 mb-2 btn dark" aria-current="page">
           <i class="fa-regular fa-user fa-2xl icon"></i>
           Ustawienia konta
         </a>
       </li>
       <li class="nav-item text-start">
-        <a href="/konto/subskrypcja" class="nav-link mt-3 mb-2 " aria-current="page">
+        <a href="/konto/subskrypcja" class="nav-link mt-3 mb-2 btn dark" aria-current="page">
           <i class="fa-solid fa-dollar-sign fa-2xl icon"></i>
           Subskrybcja
         </a>
       </li>
       <li class="nav-item text-start">
-        <a href="/konto/zostanArtysta" class="nav-link mt-3 mb-2 " aria-current="page">
+        <a href="/konto/zostanArtysta" class="nav-link mt-3 mb-2 btn dark" aria-current="page">
           <i class="fa-solid fa-microphone fa-2xl icon"></i>
           Artysta
         </a>
       </li>
       <li class="nav-item text-start">
-        <a href="/konto/zamowienia" class="nav-link mt-3 mb-2">
+        <a href="/konto/zamowienia" class="nav-link mt-3 mb-2 btn dark">
           <i class="fa-solid fa-file-invoice fa-2xl icon"></i>
           Zamówienia
         </a>
       </li>
       <li class="nav-item text-start">
-        <a href="/konto/usun" class="nav-link mt-3">
+        <a href="/konto/usun" class="nav-link mt-3 btn dark">
           <i class="fa-solid fa-trash fa-2xl icon"></i>
           Usunięcie konta
         </a>
@@ -41,7 +41,6 @@
   min-height: 100vh;
   height: 100%;
   padding-bottom: 20px;
-  border-right: 3px solid #CCCCCC;
 }
 @media (max-width: 576px) {
   .side-bar-st {
