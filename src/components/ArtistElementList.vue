be1d3b9ff8bd887d60d274f06f9cfc2c308e630b
BartoszBoguszQ <bartosz.b.bogusz@gmail.com>
2024-01-03 17:21:47 +0100

    mianowanie adminem studia i usuwanie aplikujacego

--- src/components/ArtistElementList.vue
+++ src/components/ArtistElementList.vue
@@ -3,14 +3,15 @@
     <th scope="row" class="pt-5">{{ placeRef + 1 }}</th>
     <td class="pt-5">{{ name }}</td>
     <td class="pt-5">
-      <button class="btn btn-primary me-3">Mianuj adminem</button>
-      <button class="btn btn-danger">Usuń</button>
+      <button @click="makeAdmin(userid)" class="btn btn-primary me-3">Mianuj adminem</button>
+      <button @click="deleteArtist(id)" class="btn btn-danger">Usuń</button>
     </td>
   </tr>
 </template>
 
 <script>
 import {ref} from 'vue';
+import api from "@/services/api";
 
 export default {
   props: {
@@ -34,6 +35,24 @@ export default {
       addDateRef,
       placeRef
     };
+  },
+  methods: {
+    deleteArtist(id){
+      api.delete(`/api/Studio/RemoveArtistFromStudio?artistId=${id}`)
+          .then(response => {
+            if (response.status === 200)
+              window.location.reload();
+          });
+    },
+    makeAdmin(userId){
+      api.post(`/api/Studio/MakeArtistAnAdm?artistId=${userId}`)
+          .then(response=>{
+        if (response.status === 200){
+          window.location.reload();
+        }
+      });
+
+    }
   },
 }
 </script>
